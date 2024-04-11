95739dd719d62b35c05a1357c9e3180a73458d46
Huragan11 <hubert.ragan11@gmail.com>
2024-04-11 19:06:47 +0200

    Added SpawnManager.cs, MoveLeft.cs, Updated PlayerController.cs

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,17 +7,26 @@ public class PlayerController : MonoBehaviour
     private Rigidbody _playerRigidbody;
     [SerializeField] float jumpForce;
     [SerializeField] float gravityModifier;
+    public bool isOnGround = true;
     void Start()
     {
         _playerRigidbody = GetComponent<Rigidbody>();
         Physics.gravity *= gravityModifier;
     }
-
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space))
+        if(Input.GetKeyDown(KeyCode.Space) && isOnGround)
         {
+            
             _playerRigidbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isOnGround = false;
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Ground"))
+        {
+            isOnGround = true;
         }
     }
 }

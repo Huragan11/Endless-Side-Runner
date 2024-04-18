44c949d6dff992546aea27eb5c77b47edb829ee6
Huragan11 <hubert.ragan11@gmail.com>
2024-04-18 13:20:11 +0200

    Updated PlayerController.cs, Added player animations

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,18 +7,21 @@ public class PlayerController : MonoBehaviour
     [SerializeField] float gravityModifier;
     public bool isOnGround = true;
     public bool gameOver = false;
+    private Animator _playerAnim;
+    
     void Start()
     {
         _playerRigidbody = GetComponent<Rigidbody>();
         Physics.gravity *= gravityModifier;
+        _playerAnim = GetComponent<Animator>();
     }
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        if(Input.GetKeyDown(KeyCode.Space) && isOnGround && !gameOver)
         {
-            
             _playerRigidbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isOnGround = false;
+            _playerAnim.SetTrigger("Jump_trig");
         }
     }
     private void OnCollisionEnter(Collision collision)
@@ -31,6 +34,8 @@ public class PlayerController : MonoBehaviour
         {
             gameOver = true;
             Debug.Log("Game Over!");
+            _playerAnim.SetBool("Death_b", true);
+            _playerAnim.SetInteger("DeathType_int", 1);
         }
     }
 }
